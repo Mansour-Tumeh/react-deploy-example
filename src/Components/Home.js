59a47bab18c908082b562fc4f00ac8f6e3a15e9d
Mansour-Tumeh <46924057+Mansour-Tumeh@users.noreply.github.com>
2023-08-16 11:32:12 +0200

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-            <h1> this is the Home  </h1>
+            <h1> this is the Home page for the deployes code  </h1>
            
     </div>
   )
